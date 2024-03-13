dd8593eaca2a175b76b059b03ff4ee6b7b3143a3
Karol Mac <karol200242@gmail.com>
2024-03-13 09:40:05 +0100

    just tests XD

--- src/test/java/com/restapi/vinted/repository/ClotheRepositoryTest.java
+++ src/test/java/com/restapi/vinted/repository/ClotheRepositoryTest.java
@@ -5,11 +5,9 @@ import com.restapi.vinted.entity.Clothe;
 import com.restapi.vinted.entity.User;
 import com.restapi.vinted.utils.ClotheSize;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.data.domain.Pageable;
 
 import java.math.BigDecimal;
