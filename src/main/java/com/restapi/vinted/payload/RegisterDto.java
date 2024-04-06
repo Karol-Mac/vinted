92821496f289d959517cf2cea677c510fe0629b0
Karol Mac <karol200242@gmail.com>
2024-04-06 07:31:36 +0200

    commit, before sth goes wrong XD
    (cherry picked from commit 91454de58c41bff1596a0171b7f86b6789ae16df)

--- src/main/java/com/restapi/vinted/payload/RegisterDto.java
+++ src/main/java/com/restapi/vinted/payload/RegisterDto.java
@@ -10,6 +10,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+//@NotNull
 public class RegisterDto {
     @NotEmpty
     private String name;
@@ -21,14 +22,8 @@ public class RegisterDto {
     private String email;
 
     @NotNull
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z]).+$", message = "must have at least 1 small letter and 1 uppercase letter")
-    @Pattern(regexp = ".*\\d+.*", message = "must have at least 1 number")
-    @Pattern(regexp = ".*[\\W_]+.*", message = "must have at least 1 special character")
-    @Pattern(regexp = "^.{8,20}$", message = "must be at least 8 characters (max 20)")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "must have at least: 1 small letter, 1 uppercase letter, 1 number, " +
+                    "1 special character & at least 8 characters (total) ")
     private String password;
 }
-
-
-//    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
-//            message = "must have at least: 1 small letter, 1 uppercase letter, 1 number, " +
-//                    "1 special character & at least 8 characters (total) ")
