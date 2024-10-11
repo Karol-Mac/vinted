3d148e0d64e76e0af5f6944b9962ef61432b85fe
Karol Mac <karol200242@gmail.com>
2024-10-11 21:28:44 +0200

    fix/ready to merge

--- src/main/java/com/restapi/vinted/repository/StorageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.restapi.vinted.repository;
-
-import com.restapi.vinted.entity.Storage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StorageRepository extends JpaRepository<Storage, Long> {
-
-}
