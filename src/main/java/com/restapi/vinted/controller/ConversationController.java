609c7e40c8868912d8b4b1c76d2b9de3b20ac1c4
Karol Mac <karol200242@gmail.com>
2024-10-13 17:50:11 +0200

    feat/structure for conversations created

--- src/main/java/com/restapi/vinted/controller/ConversationController.java
+++ src/main/java/com/restapi/vinted/controller/ConversationController.java
@@ -1,11 +1,14 @@
 package com.restapi.vinted.controller;
 
+import com.restapi.vinted.payload.ConversationDto;
+import com.restapi.vinted.payload.MessageDto;
 import com.restapi.vinted.service.ConversationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -17,10 +20,25 @@ public class ConversationController {
         this.conversationService = conversationService;
     }
 
-    @PostMapping("/conversation")
+    @PostMapping("/conversations")
     public ResponseEntity<Void> startConversation(@RequestParam long clotheId, Principal principal) {
 
         conversationService.startConversation(clotheId, principal.getName());
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/conversations/buying")
+    public ResponseEntity<List<ConversationDto>> getConversationsBuying(Principal principal) {
+        return ResponseEntity.ok(conversationService.getConversationsBuying(principal.getName()));
+    }
+
+    @GetMapping("/conversations/selling")
+    public ResponseEntity<List<ConversationDto>> getConversations(@RequestParam long clotheId, Principal principal) {
+        return ResponseEntity.ok(conversationService.getConversationsSelling(clotheId, principal.getName()));
+    }
+
+    @GetMapping("/conversations")
+    public ResponseEntity<List<MessageDto>> getMessages(@RequestParam long buyerId, @RequestParam long clotheId, Principal principal) {
+        return ResponseEntity.ok(conversationService.getMessages(buyerId, clotheId, principal.getName()));
     }
 }
--- src/main/java/com/restapi/vinted/service/ConversationService.java
+++ src/main/java/com/restapi/vinted/service/ConversationService.java
@@ -1,6 +1,16 @@
 package com.restapi.vinted.service;
 
+import com.restapi.vinted.payload.ConversationDto;
+import com.restapi.vinted.payload.MessageDto;
+
+import java.util.List;
+
 public interface ConversationService {
     void startConversation(long clotheId, String email);
 
+    List<ConversationDto> getConversationsBuying(String email);
+
+    List<MessageDto> getMessages(long buyerId, long clotheId, String email);
+
+    List<ConversationDto> getConversationsSelling(long clotheId, String name);
 }
