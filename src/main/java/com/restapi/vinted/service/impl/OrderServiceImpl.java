7affc6a77e74aaf905d99fef8c01f17091fe12d4
Karol Mac <karol200242@gmail.com>
2024-11-05 14:12:37 +0100

    feat/ order service implementation started

---
+++ src/main/java/com/restapi/vinted/service/impl/OrderServiceImpl.java
@@ -0,0 +1,12 @@
+package com.restapi.vinted.service.impl;
+
+import com.restapi.vinted.service.OrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public void createOrder(long clotheId, String email) {
+
+    }
+}
